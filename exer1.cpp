c73cabdc46998eeaf40f23b33649c5fb7af636ba
onigiri <onigirionta@gmail.com>
2017-11-30 19:59:43 +0300

    added comments

--- exer1.cpp
+++ exer1.cpp
@@ -5,48 +5,63 @@
 #include <stdlib.h> 
 #include <ctype.h>
 #include <algorithm>
-#include <boost/tokenizer.hpp>
+#include <sstream>
+#include <stdio.h>
+
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
 	string c;
+    int debug=0;
 	set<string> dictionary;			
 
-	ifstream dic("english-upper.10");
+	ifstream dic("english-upper.10"); //инициализация файловой переменной словаря
 
     if(!dic) {
-        cerr << "Dictionary not found." << endl;
+        cerr << "Dictionary not found." << endl; //проверка наличия файла словаря
         system("pause");
         return 1;
     }
 
 	while (!dic.eof())
 	{
-		dic >> c;
-		dictionary.insert (c);	
-    }
+		dic >> c; 
+		dictionary.insert (c);	//чтение из файла словаря в множество dictionary
+        debug++;
+    } 
+    cout << endl << "Number of words in dictionary: " << debug << endl;
 
-    ifstream input("input.txt");
+    ifstream row("input.txt"); //инициализация файловой перемнной row для текстового файла
 
-    if(!input) {
-        cerr << "File not found." << endl;
+    if(!row) {
+        cerr << "File not found." << endl; //проверка наличия файла ввода
         system("pause");
         return 1;
     }
     string instr;
-	getline (input, instr, '\0');
+	getline (row, instr, '\0'); //чтение из файла ввода в строку instr
 
-    transform(instr.begin(), instr.end(), instr.begin(), ::tolower);
-    cout << endl << instr;
+    transform(instr.begin(), instr.end(), instr.begin(), ::tolower); //перевод всей строки в нижний регистр
+    cout << endl << instr; //вывод строкию ОТЛАДКА
+    string word,inword;//строки для подсчета и для ввода новых слов
+    istringstream in(instr);
+    int j=-1; //
+    do{
+         in >> word;
+         j++;
+    } while(in);
+    cout << endl <<j;
 
-    typedef boost::tokenizer<boost::char_separator<char> > Tok;
-     boost::char_separator<char> sep; // default constructed
-     Tok tok(instr, sep);
-   for(Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end(); ++tok_iter)
-     std::cout << "<" << *tok_iter << "> ";
-   std::cout << "\n";
+    istringstream inw(instr); //
 
+    for(int i=0; i < j; i++){
+        inw >> inword;
+        cout << endl << inword;
+    }
+
+
+    cout << endl;
     system("pause");
 
 	return 0;
